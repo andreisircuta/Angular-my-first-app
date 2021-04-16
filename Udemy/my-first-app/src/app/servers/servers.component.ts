00c176c392105c803c7a1f0dec4eb5a80c170fad
andrei.sircuta <andrei.sircuta@gmail.com>
2021-04-16 17:13:22 +0300

    ngFor

--- Udemy/my-first-app/src/app/servers/servers.component.ts
+++ Udemy/my-first-app/src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server has been created';
   serverName = 'TestServer';
   serverCreated : boolean = false;
+  servers = ['testserver' , 'Testserver2'];
 
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created, the name is ' + this.serverName;
   }
 
