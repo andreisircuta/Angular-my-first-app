0dd03a2cc12623a6f02744a2b9f563f41148513d
andrei.sircuta <andrei.sircuta@gmail.com>
2021-04-16 14:58:26 +0300

    Add warning-alert

--- Udemy/my-first-app/src/app/servers/servers.component.ts
+++ Udemy/my-first-app/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]', //attribute selector
+  // selector: '.app-servers',     //class selector
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
